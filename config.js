45cfa8d66759b7417cbda6c02343f4ce90b2730b
Your Name <you@example.com>
2026-01-11 17:07:25 -0600

    update

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
   botToken: process.env.BOT_TOKEN,
   clientID: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
-  callbackURL: 'https://clearwater-network.onrender.com/auth/discord/callback',
+  callbackURL: 'https://clearwater-network.onrender.com',
   guildID: process.env.GUILD_ID,
   staffRoleID: process.env.STAFF_ROLE_ID
 };
