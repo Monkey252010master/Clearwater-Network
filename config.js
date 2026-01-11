6629e8e00f937a5d8fbff25f6c9c0dc6e424d343
Your Name <you@example.com>
2026-01-11 16:58:21 -0600

    update

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  botToken: process.env.BOT_TOKEN,
+  clientID: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  callbackURL: 'https://clearwater-network-production.up.railway.app/auth/discord/callback',
+  guildID: process.env.GUILD_ID,
+  staffRoleID: process.env.STAFF_ROLE_ID
+};
