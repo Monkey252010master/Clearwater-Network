09e976f7e5d666e021a39bfc1b47ee0b05a4c7f3
Your Name <you@example.com>
2026-01-11 19:57:03 -0600

    update

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const { Client, GatewayIntentBits } = require('discord.js');
 const fs = require('fs');
 const path = require('path');
 const config = require('./config');
-
+const { isUserStaff, isUserCAD, isUserHR } = require('./utils/roles');
 const app = express();
 
 // Needed to read POST form data
